f1d552bdca03467714a7279ad58693c02ed725c7
ycygithub <690742590@qq.com>
2016-01-07 00:14:23 +0800

    ini commit

--- algorithm-analysis/src/main/java/design/decorator/PacketBodyCreator.java
+++
@@ -1,8 +0,0 @@
-package design.decorator;
-
-public class PacketBodyCreator implements IPacketCreator {
-    @Override
-    public String handleContent() {
-        return "Content of Packet";
-    }
-}
